81fb3a06842e0cfceaca19c0ae2ff4aab8e8096f
Anna Pawlowska <apawlowska991@gmail.com>
2023-11-09 16:03:35 +0000

    increase/decrease

---
+++ day 11/EXCERCISE/start.js
@@ -0,0 +1,33 @@
+//1. pobieramy wszystkie potrzebne elementy z DOM
+
+const btnIncrease = document.querySelector('.bigger');
+const btnDecrease = document.querySelector('.smaller');
+const text = document.querySelector('p');
+
+//1b - okreslanie potrzebnych danych
+
+let textSize = 20;
+text.style.fontSize = textSize + 'px';
+text.style.color
+ = '#aaa';
+ text.style.backgroundColor = '#ddd'
+
+
+//3. Okreslenie akcji - napisanie funkcji 
+
+function textIncrease() {
+    textSize++;
+    text.style.fontSize = textSize + 'px'
+}
+
+function textDecrease(){
+    textSize--;
+    text.style.fontSize = textSize + 'px'
+}
+
+
+//2. Ustawienie nasluchiwania na przyciskach klikniecie
+
+btnIncrease.addEventListener('click', textIncrease);
+
+btnDecrease.addEventListener('click', textDecrease);
