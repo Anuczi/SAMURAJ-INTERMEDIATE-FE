e9c087e6dfb8119296e6e1bfe6c4c457ccc70648
Anna Pawlowska <apawlowska991@gmail.com>
2023-11-16 14:26:05 +0000

    burger menu

---
+++ day 13/hamburger 3/script.js
@@ -0,0 +1,10 @@
+const burger = document.querySelector(".burger");
+const iconBurger = document.querySelector(".fa-bars");
+const iconX = document.querySelector(".fa-xmark");
+const column = document.querySelector("aside");
+
+burger.addEventListener("click", function () {
+  iconBurger.classList.toggle("show");
+  iconX.classList.toggle("show");
+  column.classList.toggle("show");
+});
---
+++ day 13/hamburger jQuery/script.js
@@ -0,0 +1,14 @@
+$(".burger").on("click", function () {
+  $(".fa-solid, aside").toggleClass("show");
+});
+
+// const burger = document.querySelector(".burger");
+// const iconBurger = document.querySelector(".fa-bars");
+// const iconX = document.querySelector(".fa-xmark");
+// const column = document.querySelector("aside");
+
+// burger.addEventListener("click", function () {
+//   iconBurger.classList.toggle("show");
+//   iconX.classList.toggle("show");
+//   column.classList.toggle("show");
+// });
